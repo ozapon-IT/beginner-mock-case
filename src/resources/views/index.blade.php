a0df630ca991a792bf8c833e8b188ba24c364da5
ozawa takashi <ozapon4@gmail.com>
2024-11-11 19:48:39 +0900

    マイページ出品商品＆購入商品表示、トップページマイリスト表示機能を実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,15 +9,9 @@
 @section('content')
 <div class="tabs">
     <div class="tabs__container">
-        <a class="tabs__tab tabs__tab--active" href="{{ route('top') }}">おすすめ</a>
+        <a class="tabs__tab {{ request('tab') !== 'mylist' ? 'tabs__tab--active' : '' }}" href="{{ route('top') }}">おすすめ</a>
 
-        @guest
-        <a class="tabs__tab" href="{{ route('login') }}">マイリスト</a>
-        @endguest
-
-        @auth
-        <a class="tabs__tab" href="#?tab=mylist">マイリスト</a>
-        @endauth
+        <a class="tabs__tab {{ request('tab') === 'mylist' ? 'tabs__tab--active' : '' }}" href="{{ route('top', ['tab' => 'mylist']) }}">マイリスト</a>
     </div>
 </div>
 
